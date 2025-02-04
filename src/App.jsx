2606807fc92972b73ce74a2dc0318ea9c3c86f99
Ignacio <iggyjurado45@gmail.com>
2025-02-03 20:21:16 -0500

    lets try

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navigation from "./components/Navigation";
 import Books from "./components/Books/Books";
@@ -15,21 +15,14 @@ const App = () => {
     <Router>
       <Navigation />
       <Routes>
+        <Route path="/" element={!isAuthenticated ? <Login /> : <Navigate to="/account" />} />
+
         <Route path="/books" element={<Books />} />
         <Route path="/books/:id" element={<SingleBook />} />
-        <Route
-          path="/login"
-          element={!isAuthenticated ? <Login /> : <Account />}
-        />
-        <Route
-          path="/register"
-          element={!isAuthenticated ? <Register /> : <Account />}
-        />
-        <Route
-          path="/account"
-          element={isAuthenticated ? <Account /> : <Login />}
-        />
-        <Route path="*" element={<Books />} />
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/account" />} />
+        <Route path="/register" element={!isAuthenticated ? <Register /> : <Navigate to="/account" />} />
+        <Route path="/account" element={isAuthenticated ? <Account /> : <Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
